0c44873a45cb1d3c00a9a9203bbb6b9dd9251771
Samuel Bustamante <samubustamante@hotmail.com>
2022-04-22 21:47:49 -0300

    Ejercicio 2 con interfaz grafica terminado

--- Main.py
+++ Main.py
@@ -1,80 +1,166 @@
 import datetime
 import random
 import pandas as pd
+
+# Eventos.
+# 1) Llegada de un cliente al sistema
+# 2) Fin del servicio.
+# 3) Llegada del servidor (regreso al PS)
+# 4) Salida del servidor (abandono del PS).
+# Variables de estado.
+# 1) Estado de ocupado (ps=1) o libre (ps=0) del puesto de servicio.
+# 2) Cantidad de clientes en cola (q).
+# 3) Presencia (S=1) o ausencia(S=0) del servidor.
 class Sistema():
     
     #* Inicializacion de variables
-    def __init__(self, cola, ps, hora_actual_inicial, hora_llegada_cliente_inicial, hora_fin_servicio_inicial, iteraciones, duracion_servicio_min, duracion_servicio_max, intervalo_cliente_min, intervalo_cliente_max):
-        self.iteraciones           = iteraciones
-        self.cola                  = cola
-        self.ps                    = ps
-        self.data                  = []
-        self.hora_actual           = datetime.datetime(2022, 6, 22, int(hora_actual_inicial[0]), int(hora_actual_inicial[1]), int(hora_actual_inicial[2]))
-        self.hora_llegada_cliente  = datetime.datetime(2022, 6, 22, int(hora_llegada_cliente_inicial[0]), int(hora_llegada_cliente_inicial[1]), int(hora_llegada_cliente_inicial[2]))
-        self.hora_fin_servicio     = datetime.datetime(2022, 6, 22, int(hora_fin_servicio_inicial[0]), int(hora_fin_servicio_inicial[1]), int(hora_fin_servicio_inicial[2]))
-        self.duracion_servicio_min = duracion_servicio_min
-        self.duracion_servicio_max = duracion_servicio_max
-        self.intervalo_cliente_min = intervalo_cliente_min
-        self.intervalo_cliente_max = intervalo_cliente_max
-        # self.duracion_servicio     = datetime.timedelta(seconds=duracion_servicio)
-        # self.intervalo_por_cliente = datetime.timedelta(seconds=intervalo_por_cliente) 
+    def __init__(self, 
+                 cola, 
+                 ps, 
+                 hora_actual_inicial, 
+                 hora_llegada_cliente_inicial, 
+                 hora_fin_servicio_inicial, 
+                 iteraciones, 
+                 duracion_servicio_min, 
+                 duracion_servicio_max, 
+                 intervalo_cliente_min, 
+                 intervalo_cliente_max,
+                 hora_comienzo_descanso_inicial,
+                 hora_vuelta_trabajo_inicial,
+                 s,
+                 duracion_descanso_min, 
+                 duracion_descanso_max, 
+                 duracion_trabajo_min, 
+                 duracion_trabajo_max):
+        self.s                               = s
+        self.hora_comienzo_descanso          = datetime.datetime(year=2022, 
+                                                                 month=6, 
+                                                                 day=22,
+                                                                 hour=int(hora_comienzo_descanso_inicial[0]),
+                                                                 minute=int(hora_comienzo_descanso_inicial[1]),
+                                                                 second=int(hora_comienzo_descanso_inicial[2]))
+        self.hora_vuelta_trabajo             = datetime.datetime(year=2023, 
+                                                                 month=6, 
+                                                                 day=22,
+                                                                 hour=int(hora_vuelta_trabajo_inicial[0]),
+                                                                 minute=int(hora_vuelta_trabajo_inicial[1]),
+                                                                 second=int(hora_vuelta_trabajo_inicial[2]))
+        self.iteraciones                     = iteraciones
+        self.cola                            = cola
+        self.ps                              = ps
+        self.hora_actual                     = datetime.datetime(year=2022, 
+                                                                 month=6, 
+                                                                 day=22, 
+                                                                 hour=int(hora_actual_inicial[0]), 
+                                                                 minute=int(hora_actual_inicial[1]), 
+                                                                 second=int(hora_actual_inicial[2]))
+        self.hora_llegada_cliente            = datetime.datetime(year=2022, 
+                                                                 month=6, 
+                                                                 day=22, 
+                                                                 hour=int(hora_llegada_cliente_inicial[0]), 
+                                                                 minute=int(hora_llegada_cliente_inicial[1]), 
+                                                                 second=int(hora_llegada_cliente_inicial[2]))
+        self.hora_fin_servicio               = datetime.datetime(year=2022, 
+                                                                 month=6, 
+                                                                 day=22, 
+                                                                 hour=int(hora_fin_servicio_inicial[0]), 
+                                                                 minute=int(hora_fin_servicio_inicial[1]), 
+                                                                 second=int(hora_fin_servicio_inicial[2]))
+        self.duracion_servicio_min           = duracion_servicio_min
+        self.duracion_servicio_max           = duracion_servicio_max
+        self.intervalo_cliente_min           = intervalo_cliente_min
+        self.intervalo_cliente_max           = intervalo_cliente_max
+        self.intervalo_duracion_trabajo_min  = duracion_trabajo_min
+        self.intervalo_duracion_trabajo_max  = duracion_trabajo_max
+        self.intervalo_duracion_descanso_min = duracion_descanso_min
+        self.intervalo_duracion_descanso_max = duracion_descanso_max
+        
+        self.data                            = [] #Arreglo para guardar valores ingresados por teclado
+        self.hs_comienzo_descanso_flag       = False
+        self.hs_fin_servicio_flag            = False
+        self.hs_vuelta_trabajo_flag          = False
+        
+    def salida_servidor(self):
+        
+        self.hora_actual               = self.hora_comienzo_descanso
+        self.s                         = 0
+        self.duracion_descanso         = datetime.timedelta(seconds=random.randint(self.intervalo_duracion_descanso_min, self.intervalo_duracion_descanso_max)) 
+        self.hora_vuelta_trabajo       = self.hora_comienzo_descanso + self.duracion_descanso
+        if self.ps != 0:
+            self.hora_fin_servicio    += self.duracion_descanso
+        self.hora_comienzo_descanso   += datetime.timedelta(hours=5)
+        self.hs_comienzo_descanso_flag = True
+        self.hs_vuelta_trabajo_flag    = False
+        
+    def regreso_del_servidor(self):
+        self.hora_actual               = self.hora_vuelta_trabajo
+        self.s                         = 1   
+        self.duracion_trabajo       = datetime.timedelta(seconds=random.randint(self.intervalo_duracion_trabajo_min, self.intervalo_duracion_trabajo_max)) 
+        self.hora_comienzo_descanso    = self.hora_actual + self.duracion_trabajo
+        self.hora_vuelta_trabajo      += datetime.timedelta(hours=5)
+        self.hs_vuelta_trabajo_flag    = True
+        self.hs_comienzo_descanso_flag = False
+        
         
     #* Metodo para simular evento de la entrada de un cliente al sistema
     def llegada_cliente(self):
-        self.hora_actual            = self.hora_llegada_cliente
+        self.hora_actual                = self.hora_llegada_cliente
         if self.ps == 0:
-            self.ps                 = 1
-            self.hora_fin_servicio  =  self.hora_llegada_cliente + self.duracion_servicio
+            self.ps                     = 1
+            if self.s == 0:
+                self.hora_fin_servicio  =  self.hora_vuelta_trabajo + self.duracion_servicio
+            else:
+                self.hora_fin_servicio  = self.hora_llegada_cliente + self.duracion_servicio
+            self.hs_fin_servicio_flag   = False
         else:
-            self.cola              += 1
+            self.cola                  += 1
         
-        self.hora_llegada_cliente  += self.intervalo_por_cliente
+        self.hora_llegada_cliente      += self.intervalo_por_cliente
 
     #* Metddo para simular evento de la salida de un cliente al sistema
     def fin_de_servicio(self):
-        self.hora_actual            = self.hora_fin_servicio
+        self.hora_actual              = self.hora_fin_servicio
         self.ps = 0
         if(self.cola > 0):
-            self.ps                 = 1
-            self.cola              -= 1
-            self.hora_fin_servicio += self.duracion_servicio
+            self.ps                   = 1
+            self.cola                -= 1
+            self.hora_fin_servicio   += self.duracion_servicio
         else:
-            self.hora_fin_servicio  = None
+            self.hora_fin_servicio   += datetime.timedelta(hours=5)
+            self.hs_fin_servicio_flag = True
     
     #* Metodo para elegir el evento que se va a simular
     def elegirEvento(self):
         for i in range(0,self.iteraciones):
             
-            #? Segundos con numeros aleatorios
-            #? Tiempo de llegada minimo  = 40sec
-            #? Tiempo de llegada maximo  = 60sec
-            #? Tiempo de servicio minimo = 30sec
-            #? Tiempo de servicio minimo = 50sec
-            self.duracion_servicio     = datetime.timedelta(seconds=random.randint(self.duracion_servicio_min, self.duracion_servicio_max))
-            self.intervalo_por_cliente = datetime.timedelta(seconds=random.randint(self.intervalo_cliente_min, self.intervalo_cliente_max))
+            self.duracion_servicio      = datetime.timedelta(seconds=random.randint(self.duracion_servicio_min, self.duracion_servicio_max))
+            self.intervalo_por_cliente  = datetime.timedelta(seconds=random.randint(self.intervalo_cliente_min, self.intervalo_cliente_max))
+             
+            hora_fin_servicio_aux       = ('--------' if self.hs_fin_servicio_flag else self.hora_fin_servicio.time())
+            hora_comienzo_descanso_aux  = ('--------' if self.hs_comienzo_descanso_flag else self.hora_comienzo_descanso.time())
+            hora_vuelta_trabajo_aux     = ('--------' if self.hs_vuelta_trabajo_flag else self.hora_vuelta_trabajo.time())
             
-            if self.hora_fin_servicio == None:
-                self.data.append([self.hora_actual.time(),
-                      self.hora_llegada_cliente.time(),
-                      '--------',
-                      self.cola,
-                      self.ps,
-                      self.duracion_servicio.seconds,
-                      self.intervalo_por_cliente.seconds])
-            else:
-                self.data.append([self.hora_actual.time(),
-                      self.hora_llegada_cliente.time(),
-                      self.hora_fin_servicio.time(),
-                      self.cola,
-                      self.ps,
-                      self.duracion_servicio.seconds,
-                      self.intervalo_por_cliente.seconds])
-         
+            self.data.append([self.hora_actual.time(),
+                    self.hora_llegada_cliente.time(),
+                    hora_fin_servicio_aux,
+                    self.cola,
+                    self.ps,
+                    self.duracion_servicio.seconds,
+                    self.intervalo_por_cliente.seconds,
+                    hora_comienzo_descanso_aux,
+                    hora_vuelta_trabajo_aux,
+                    self.s])
+            
                          
-            if(self.hora_fin_servicio == None or self.hora_llegada_cliente <= self.hora_fin_servicio):
+            if((self.hora_llegada_cliente <= self.hora_fin_servicio) and (self.hora_llegada_cliente <= self.hora_comienzo_descanso) and (self.hora_llegada_cliente <= self.hora_vuelta_trabajo)):
                 self.llegada_cliente()
-            else:
+            elif ((self.hora_fin_servicio <= self.hora_comienzo_descanso) and (self.hora_fin_servicio <= self.hora_vuelta_trabajo)):
                 self.fin_de_servicio()
+            elif (self.hora_comienzo_descanso <= self.hora_vuelta_trabajo):
+                self.salida_servidor()
+            else :
+                self.regreso_del_servidor()
+                
                 
         return self.data
 
--- MainWindow.py
+++ MainWindow.py
@@ -47,30 +47,47 @@ class Window:
         self.tabla_simulacion.configure(xscrollcommand=self.scrollbarTabla.set)
 
         #NUMERO DE COLUMNAS
-        self.tabla_simulacion["columns"] = ("iteracion", "horaActual", "horaProxCliente", "horaFinServicio", "Q", "PS", "duracionServicio", "intervaloLlegadaCliente")
+        self.tabla_simulacion["columns"] = ("iteracion", 
+                                            "horaActual", 
+                                            "horaProxCliente", 
+                                            "horaFinServicio", 
+                                            "horaComienzoDescanso", 
+                                            "horaVueltaTrabajo",
+                                            "Q", 
+                                            "PS", 
+                                            "S",
+                                            "intervaloLlegadaCliente", 
+                                            "duracionServicio"
+                                            )
 
         #DEFINIENDO HEADING
         self.tabla_simulacion['show'] = 'headings'
 
         #AGREGAR COLUMNAS
         self.tabla_simulacion.column("iteracion",                width=50,  anchor='center')
-        self.tabla_simulacion.column("horaActual",               width=150, anchor='center')
-        self.tabla_simulacion.column("horaProxCliente",          width=150, anchor='center')
-        self.tabla_simulacion.column("horaFinServicio",          width=150, anchor='center')
-        self.tabla_simulacion.column("Q",                        width=150, anchor='center')
-        self.tabla_simulacion.column("PS",                       width=150, anchor='center')
-        self.tabla_simulacion.column("duracionServicio",         width=150, anchor='center')
-        self.tabla_simulacion.column("intervaloLlegadaCliente",  width=150, anchor='center')
+        self.tabla_simulacion.column("horaActual",               width=130, anchor='center')
+        self.tabla_simulacion.column("horaProxCliente",          width=130, anchor='center')
+        self.tabla_simulacion.column("horaFinServicio",          width=130, anchor='center')
+        self.tabla_simulacion.column("Q",                        width=50,  anchor='center')
+        self.tabla_simulacion.column("PS",                       width=50,  anchor='center')
+        self.tabla_simulacion.column("duracionServicio",         width=130, anchor='center')
+        self.tabla_simulacion.column("intervaloLlegadaCliente",  width=130, anchor='center')
+        self.tabla_simulacion.column("horaComienzoDescanso",     width=130, anchor='center')
+        self.tabla_simulacion.column("horaVueltaTrabajo",        width=130, anchor='center')
+        self.tabla_simulacion.column("S",                        width=50,  anchor='center')
 
         #HEADINGS COLUMNAS
         self.tabla_simulacion.heading("iteracion",               text="N°")
-        self.tabla_simulacion.heading("horaActual",              text="Hora Actual")
-        self.tabla_simulacion.heading("horaProxCliente",         text="Hora llegada prox cliente")
-        self.tabla_simulacion.heading("horaFinServicio",         text="Hora fin de servicio")
+        self.tabla_simulacion.heading("horaActual",              text="Hs Actual")
+        self.tabla_simulacion.heading("horaProxCliente",         text="Hs llegada prox cliente")
+        self.tabla_simulacion.heading("horaFinServicio",         text="Hs fin de servicio")
         self.tabla_simulacion.heading("Q",                       text="Q")
         self.tabla_simulacion.heading("PS",                      text="PS")
         self.tabla_simulacion.heading("duracionServicio",        text="Duración Servicio")
         self.tabla_simulacion.heading("intervaloLlegadaCliente", text="Tiempo llegada")
+        self.tabla_simulacion.heading("horaComienzoDescanso",    text="Hs comienzo descsanso")
+        self.tabla_simulacion.heading("horaVueltaTrabajo",       text="Hs Vuelta Trabajo")
+        self.tabla_simulacion.heading("S",                       text="S")
 
         self.lblCola                            = ttk.Label(self.root, text='Valor de cola inicial:', font=('Arial', 10))
         self.lblCola.grid(row=2, column=0, sticky="ens", pady=8)
@@ -116,20 +133,51 @@ class Window:
         self.entryIteraciones                   = tk.Entry(self.root)
         self.entryIteraciones.grid(row=5, column=3, sticky="ens", pady=8)
         
+        self.lbl_duracion_descanso                = ttk.Label(self.root, text='Duracion descanso minimo/maximo (segundos):', font=('Arial', 10))
+        self.lbl_duracion_descanso.grid(row=6, column=0, sticky="ens", pady=8)
+        self.entry_intervalo_duracion_descanso_minimo       = tk.Entry(self.root, width=8)
+        self.entry_intervalo_duracion_descanso_minimo.grid(row=6, column=1, sticky="ns", pady=8)
+        self.entry_intervalo_duracion_descanso_maximo              = tk.Entry(self.root, width=8)
+        self.entry_intervalo_duracion_descanso_maximo.grid(row=6, column=1, sticky="ens", pady=8)
+        
+        self.lbl_duracion_trabajo                = ttk.Label(self.root, text='Duracion trabajo minimo/maximo (segundos):', font=('Arial', 10))
+        self.lbl_duracion_trabajo.grid(row=6, column=2, sticky="ens", pady=8)
+        self.entry_intervalo_duracion_trabajo_minimo       = tk.Entry(self.root, width=8)
+        self.entry_intervalo_duracion_trabajo_minimo.grid(row=6, column=3, sticky="ns", pady=8)
+        self.entry_intervalo_duracion_trabajo_maximo              = tk.Entry(self.root, width=8)
+        self.entry_intervalo_duracion_trabajo_maximo.grid(row=6, column=3, sticky="ens", pady=8)
+        
+        self.lbl_s                    = ttk.Label(self.root, text='S:', font=('Arial', 10))
+        self.lbl_s.grid(row=7, column=0, sticky="ens", pady=8)
+        self.entry_s                   = tk.Entry(self.root)
+        self.entry_s.grid(row=7, column=1, sticky="ens", pady=8)
+        
+        self.lbl_hora_comienzo_descanso_inicial          = ttk.Label(self.root, text='Hora comienzo descanso inicial (h:m:s):', font=('Arial', 10))
+        self.lbl_hora_comienzo_descanso_inicial.grid(row=7, column=2, sticky="ens", pady=8)
+        self.entry_hora_comienzo_descanso_inicial        = tk.Entry(self.root)
+        self.entry_hora_comienzo_descanso_inicial.grid(row=7, column=3, sticky="ens", pady=8)
+        
+        self.lbl_hora_vuelta_trabajo_inicial          = ttk.Label(self.root, text='Hora vuelta al trabajo inicial (h:m:s):', font=('Arial', 10))
+        self.lbl_hora_vuelta_trabajo_inicial.grid(row=8, column=0, sticky="ens", pady=8)
+        self.entry_hora_vuelta_trabajo_inicial        = tk.Entry(self.root)
+        self.entry_hora_vuelta_trabajo_inicial.grid(row=8, column=1, sticky="ens", pady=8)
+        
         #Boton para iniciar simulacion y llenar la tabla
         self.btnIniciar = tk.Button(self.root, text="Iniciar simulación", command=self.llenartabla_simulacion)
         self.btnIniciar.grid(
-            row    = 6, 
+            row    = 8, 
             column = 3, 
             sticky = "nse",
             pady=8)
-        #!LLeno la tabla de productos
-        # self.llenartabla_simulacion()
 
         self.root.tk.mainloop()
 
     def llenartabla_simulacion(self):
         self.tabla_simulacion.delete(*self.tabla_simulacion.get_children())
+        
+        hora_fin_servicio_aux = ("------" if self.entryHoraFinServicioInicial.get() == "" else self.entryHoraFinServicioInicial.get().split(sep=":"))
+        
+        
         sistema    = Sistema(
             int(self.entryCola.get()), 
             int(self.entryPS.get()), 
@@ -140,9 +188,16 @@ class Window:
             int(self.entryDuracionServicio_minimo.get()),
             int(self.entryDuracionServicio_maximo.get()),
             int(self.entryIntervaloCliente_minimo.get()),
-            int(self.entryIntervaloCliente_maximo.get())
+            int(self.entryIntervaloCliente_maximo.get()),
+            self.entry_hora_comienzo_descanso_inicial.get().split(sep=":"),
+            self.entry_hora_vuelta_trabajo_inicial.get().split(sep=":"),
+            int(self.entry_s.get()),
+            int(self.entry_intervalo_duracion_descanso_minimo.get()),
+            int(self.entry_intervalo_duracion_descanso_maximo.get()),
+            int(self.entry_intervalo_duracion_trabajo_minimo.get()),
+            int(self.entry_intervalo_duracion_trabajo_maximo.get()),
         )
-
+        
         datosTabla = sistema.elegirEvento()
         for indice,dato in reversed(list(enumerate(datosTabla))):
             if indice % 2 == 0:
@@ -153,8 +208,11 @@ class Window:
                                             dato[0],
                                             dato[1],
                                             dato[2],
+                                            dato[7],
+                                            dato[8],
                                             dato[3],
                                             dato[4],
+                                            dato[9],
                                             dato[5],
                                             dato[6]
                                         ),
@@ -168,8 +226,11 @@ class Window:
                                             dato[0],
                                             dato[1],
                                             dato[2],
+                                            dato[7],
+                                            dato[8],
                                             dato[3],
                                             dato[4],
+                                            dato[9],
                                             dato[5],
                                             dato[6]
                                         ),
