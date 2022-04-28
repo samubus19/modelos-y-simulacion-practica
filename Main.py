0a0e592684ff7f9b750e4df52875ec27eb89760b
Matias Samuel Bustamante <samuelbustamante999@gmail.com>
2022-04-28 19:10:25 -0300

    Ejercicio 4 resuelto con valores fijos por consola

--- Main.py
+++ Main.py
@@ -5,81 +5,111 @@ class Tabla():
     
     #* Inicializacion de variables
     def __init__(self) -> None:
-        self.s                     = 1
-        self.cola                  = 3
-        self.ps                    = 1
-        self.data                  = []
-        self.hora_actual           = datetime.datetime(2022, 6, 22, 8, 0, 0)
-        self.hora_llegada_cliente  = datetime.datetime(2022, 6, 22, 8, 5, 0)
-        self.hora_fin_servicio     = datetime.datetime(2022, 6, 22, 8, 3, 0)
-        self.duracion_servicio     = datetime.timedelta(seconds=40)
-        self.intervalo_por_cliente = datetime.timedelta(seconds=45)
+        self.cola_a                  = 0
+        self.cola_b                  = 0
+        self.ps                      = 0
+        self.hora_actual             = datetime.datetime(year = datetime.datetime.now().year, 
+                                                       month  = datetime.datetime.now().month, 
+                                                       day    = datetime.datetime.now().day, 
+                                                       hour   = 8, 
+                                                       minute = 0, 
+                                                       second = 0)
+         
+        self.hora_llegada_cliente_a  = datetime.datetime(year = datetime.datetime.now().year, 
+                                                       month  = datetime.datetime.now().month, 
+                                                       day    = datetime.datetime.now().day, 
+                                                       hour   = 8, 
+                                                       minute = 7, 
+                                                       second = 0)
+         
+        self.hora_llegada_cliente_b  = datetime.datetime(year = datetime.datetime.now().year, 
+                                                       month  = datetime.datetime.now().month, 
+                                                       day    = datetime.datetime.now().day, 
+                                                       hour   = 8, 
+                                                       minute = 7, 
+                                                       second = 0)
+         
+        self.hora_fin_servicio       = datetime.datetime(year = datetime.datetime.now().year, 
+                                                       month  = datetime.datetime.now().month, 
+                                                       day    = datetime.datetime.now().day,
+                                                       hour   = 8, 
+                                                       minute = 8,
+                                                       second = 0)
         
-    def llegada_del_servidor(self):
-        pass
-    
-    def salida_del_dervidor(self):
-        pass
+        self.duracion_servicio       = datetime.timedelta(minutes=2)
+        self.intervalo_por_cliente_a = datetime.timedelta(minutes=7)
+        self.intervalo_por_cliente_b = datetime.timedelta(minutes=3)
+        self.data                    = []
+        #Variables Auxiliares
+        self.flag_fin_servicio       = False
+
         
     #* Metodo para simular evento de la entrada de un cliente al sistema
-    def llegada_cliente_al_sistema(self):
-        self.hora_actual            = self.hora_llegada_cliente
+    def llegada_cliente_a(self):
+        self.hora_actual            = self.hora_llegada_cliente_a
         if self.ps == 0:
             self.ps                 = 1
-            self.hora_fin_servicio  =  self.hora_llegada_cliente + self.duracion_servicio
+            self.hora_fin_servicio  =  self.hora_llegada_cliente_a + self.duracion_servicio
         else:
-            self.cola              += 1
+            self.cola_a            += 1
         
-        self.hora_llegada_cliente  += self.intervalo_por_cliente
+        self.hora_llegada_cliente_a  += self.intervalo_por_cliente_a
+        
+         #* Metodo para simular evento de la entrada de un cliente al sistema
+    def llegada_cliente_b(self):
+        self.hora_actual            = self.hora_llegada_cliente_b
+        if self.ps == 0:
+            if self.cola_a > 0:
+                self.cola_b += 1
+            else:
+                self.ps                 = 1
+                self.hora_fin_servicio  =  self.hora_llegada_cliente_b + self.duracion_servicio
+        else:
+            self.cola_b            += 1
+        
+        self.hora_llegada_cliente_b  += self.intervalo_por_cliente_b
 
     #* Metddo para simular evento de la salida de un cliente al sistema
     def fin_de_servicio(self):
         self.hora_actual            = self.hora_fin_servicio
         self.ps = 0
-        if(self.cola > 0):
+        if(self.cola_a > 0):
             self.ps                 = 1
-            self.cola              -= 1
-            self.hora_fin_servicio += self.duracion_servicio
+            self.cola_a            -= 1
+            self.hora_fin_servicio  = self.hora_actual + self.duracion_servicio
         else:
-            #? Preguntar que hacer con la hora de fin de servicio
-            #? le asgino un valor grande a hora de fin de servicio para que se ejecute el otro evento
-            self.hora_fin_servicio  = None
+            if self.cola_b > 0:
+                self.cola_b -= 1
+                self.ps      = 1
+                self.hora_fin_servicio = self.hora_actual + self.duracion_servicio
+            else:
+                self.hora_fin_servicio += datetime.timedelta(hours=10)
+                self.flag_fin_servicio  = True
     
     #* Metodo para elegir el evento que se va a simular
     def elegirEvento(self):
-        for i in range(0,25):
+        for i in range(0,20):
             
-            #? Segundos con numeros aleatorios
-            #? Tiempo de llegada minimo  = 40sec
-            #? Tiempo de llegada maximo  = 60sec
-            #? Tiempo de servicio minimo = 30sec
-            #? Tiempo de servicio minimo = 50sec
-            # self.duracion_servicio     = datetime.timedelta(seconds=random.randint(30,50))
-            # self.intervalo_por_cliente = datetime.timedelta(seconds=random.randint(40,60))
-            
-            hora_fin_servicio_aux = ('--------' if self.hora_fin_servicio == None else self.hora_fin_servicio.time())
+            hora_fin_servicio_aux = ('--------' if self.flag_fin_servicio else self.hora_fin_servicio.time())
             
             self.data.append([self.hora_actual.time(),
-                      self.hora_llegada_cliente.time(),
+                      self.hora_llegada_cliente_a.time(),
+                      self.hora_llegada_cliente_b.time(),
                       hora_fin_servicio_aux,
-                      self.cola,
+                      self.cola_a,
+                      self.cola_b,
                       self.ps,
                       self.duracion_servicio.seconds,
-                      self.intervalo_por_cliente.seconds])
+                      ])
          
-            # if(self.hora_fin_servicio.time() == self.hora_llegada_cliente.time()):
-            #     #? Elijo cual evento ejecutar
-            #     self.hora_actual        = self.hora_llegada_cliente
-            #     self.llegada_cliente()
-            #     # self.hora_actual        = self.hora_fin_servicio
-            #     # self.fin_de_servicio()
-                
-            if(self.hora_fin_servicio == None or (self.hora_llegada_cliente <= self.hora_fin_servicio)):
-                self.llegada_cliente_al_sistema()
+            if(self.hora_llegada_cliente_a <= self.hora_llegada_cliente_b and self.hora_llegada_cliente_a <= self.hora_fin_servicio):
+                self.llegada_cliente_a()
+            elif(self.hora_llegada_cliente_b <= self.hora_fin_servicio):
+                self.llegada_cliente_b()
             else:
                 self.fin_de_servicio()
                 
-        print(pd.DataFrame(data=self.data, columns=['|Hora Actual', '|Hora prox cliente', '|Hora fin serv', '|Q', '|PS', '|Tiempo serv', '|Llegada prox cliente']))
+        print(pd.DataFrame(data=self.data, columns=['|Hora Actual', '|Hora prox cliente A', '|Hora prox cliente B', '|Hora fin serv', '|Qa', '|Qb', '|PS', '|Tiempo serv']))
 
 
 sistema = Tabla()
